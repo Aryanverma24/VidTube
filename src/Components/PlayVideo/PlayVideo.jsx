3f119f6c2661600032b753e41251761518f104df
aryan verma <aryanv2204@gmail.com>
2024-06-07 02:26:18 +0530

    fixed playvideo bugs

--- src/Components/PlayVideo/PlayVideo.jsx
+++ src/Components/PlayVideo/PlayVideo.jsx
@@ -10,39 +10,31 @@ import user_profile from "../../assets/images/user_profile.jpg";
 import { Api_key } from "../../data";
 import { valueConverter } from "../../data";
 import moment from "moment";
-import { useParams } from "react-router-dom";
-
-
 
 const PlayVideo = ({videoId}) => {
-
-  const {videoId2} = useParams
 
   const [apiData,setApiData] = useState(null);
   const [channelData,setChannelData] = useState(null);
   const [commData,setCommData] = useState([]);
 
+  const fetchVideoData = async () => {
 
-  const fetchVideoData = async() =>{
-      // fetching videos data
-
-      const videoDetails_url = `https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&videoCategoryId=${videoId}&key=${Api_key}`;
-      await fetch(videoDetails_url).then(response=>response.json()).then(data=>setApiData(data.items[2]))
+    const videoDetails_url = `https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&id=${videoId}&key=${Api_key}`;
+    await fetch(videoDetails_url).then(res=>res.json()).then(data => setApiData(data.items[0]));
   }
 
-  const fetchOtherData = async() =>{
-    // fetch Channel data..
-    const channel_url = `https://youtube.googleapis.com/youtube/v3/channels?part=snippet%2CcontentDetails%2Cstatistics&id=${apiData.snippet.channelId}&key=${Api_key}`
+  const fetchOtherData = async () => {
 
-    await fetch(channel_url).then(res=>res.json()).then(data=>setChannelData(data.items[2]))
+    const channel_url = `https://youtube.googleapis.com/youtube/v3/channels?part=snippet%2CcontentDetails%2Cstatistics&id=${apiData.snippet.channelId}&key=${Api_key}`;
+    await fetch(channel_url).then(res => res.json()).then(data => setChannelData(data.items[0])); 
 
-    const comment_url = `https://youtube.googleapis.com/youtube/v3/commentThreads?part=snippet%2Creplies&videoId=${videoId}&key=${Api_key}`;
-    await fetch(comment_url).then(res=>res.json()).then(data=>setCommData(data.items))
+    const comment_url =  `https://youtube.googleapis.com/youtube/v3/commentThreads?part=snippet%2Creplies&videoId=${videoId}&key=${Api_key}`;
+    await fetch(comment_url).then(res => res.json()).then(data => setCommData(data.items));
   }
 
   useEffect(()=> {
     fetchVideoData();
-  },[videoId2])
+  },[videoId])
 
   useEffect(() =>{
     fetchOtherData();
@@ -50,7 +42,7 @@ const PlayVideo = ({videoId}) => {
 
   return (
     <div className="play-video">
-      {/* <video src={video1} controls autoPlay muted></video> */}
+      
       <iframe src={`https://www.youtube.com/embed/${videoId}?autoplay=1`}  frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
 
       <h3>{apiData?apiData.snippet.title:"Title Here..."}</h3>
@@ -87,12 +79,12 @@ const PlayVideo = ({videoId}) => {
               <div key={index} className="comments">
               <img src={item.snippet.topLevelComment.snippet.authorProfileImageUrl} alt="" />
               <div>
-                <h3>{item.snippet.topLevelComment.snippet.authorDisplayName}<span>1 day ago</span></h3>
+                <h3>{item.snippet.topLevelComment.snippet.authorDisplayName}<span>{moment(item.snippet.topLevelComment.snippet.publishedAt).fromNow()}</span></h3>
                 <p>{item.snippet.topLevelComment.snippet.textDisplay}</p>
     
                 <div className="comment-action">
-                  <img src={valueConverter(item.snippet.topLevelComment.snippet.likeCount)} alt="" />
-                  <span>244</span>
+                  <img src={like} alt="" />
+                  <span>{valueConverter(item.snippet.topLevelComment.snippet.likeCount)}</span>
                   <img src={dislike} alt="" />
                   <span>2</span>
                 </div>
--- src/Components/Recommended/Recommended.jsx
+++ src/Components/Recommended/Recommended.jsx
@@ -1,13 +1,5 @@
 import React, { useEffect, useState } from 'react'
 import './Recommended.css'
-import thumbnail1 from '../../assets/images/thumbnail1.png'
-import thumbnail2 from '../../assets/images/thumbnail2.png'
-import thumbnail3 from '../../assets/images/thumbnail3.png'
-import thumbnail4 from '../../assets/images/thumbnail4.png'
-import thumbnail5 from '../../assets/images/thumbnail5.png'
-import thumbnail6 from '../../assets/images/thumbnail6.png'
-import thumbnail7 from '../../assets/images/thumbnail7.png'
-import thumbnail8 from '../../assets/images/thumbnail8.png'
 import { Api_key } from '../../data'
 import { valueConverter } from '../../data';
 import { Link } from 'react-router-dom'
@@ -16,7 +8,7 @@ const Recommended = ({categoryId}) => {
   const [apiData,setApiData] = useState([]);
 
   const fetchData = async() =>{
-    const relatedVideoUrl = `https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=15&regionCode=US&videoCategoryId=${categoryId}&key=${Api_key}`;
+    const relatedVideoUrl = `https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=25&regionCode=US&videoCategoryId=${categoryId}&key=${Api_key}`;
 
      await fetch(relatedVideoUrl).then(res=>res.json()).then(data=>setApiData(data.items));
   }
